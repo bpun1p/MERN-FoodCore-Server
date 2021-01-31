d507b67740fd1fca48e426df300b399811fa53e3
bpun1p <barry_pun@hotmail.com>
2021-01-31 00:12:01 -0800

    dbURI towards mongo atlas and gitignore ssh keys

--- server/server.js
+++ server/server.js
@@ -11,7 +11,7 @@ const bodyParser = require('body-parser');
 app.use(cookieParser()); // parse the cookie stored in the web browser
 app.use(bodyParser({ limit: '50mb' }));
 
-const dbURI = 'mongodb://bpun1p:Guy123su@localhost:27017';
+const dbURI = 'mongodb+srv://FoodCore:Guy123su@cluster0.amii2.mongodb.net/FoodCore?retryWrites=true&w=majority';
 
 // connect to mongodb
 mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
@@ -26,5 +26,5 @@ const usersRouter = require('./routes/usersRoute');
 app.use(cors({ origin: true, credentials: true }));
 
 // Api routes
-app.use('/user', usersRouter);
-app.use('/', allRecipeRouter);
+app.use('/api/user', usersRouter);
+app.use('/api/', allRecipeRouter);
